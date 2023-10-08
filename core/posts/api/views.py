c207c5f7238935bf19661e486683c9aed778d8a9
hideonbunny <71028109+hideonbunny@users.noreply.github.com>
2023-10-08 15:16:23 -0400

    Delete core/posts directory

--- core/posts/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from ..models import Post
-from .serializers import PostSerializer
-
-
-class PostViewSet(ModelViewSet):
-    serializer_class = PostSerializer
-    queryset = Post.objects.all()
